87ce3a807e325cac8183cb84a37748d991c5bcd1
maxkrieger <maxkrieger@users.noreply.github.com>
2021-11-23 11:13:14 -0800

    bump patch

---
+++ lib/widgets/search_results_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchResultsList extends StatelessWidget {
+  const SearchResultsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
